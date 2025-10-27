f164f172de4d2e29ab03427705c9ccd7ad1fb42c
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-27 18:08:35 +0100

    [PART 1] finished basic text-based and graphical interfaces

--- scrabble.py
+++ scrabble.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 """
 -----------------------------------------------------------------------------
-i11_XXXX_YYYY_projet.py : CR projet « Scrabble », groupe IMA-04
+i11_shirkunov_barlubayeva_projet.py : CR projet « Scrabble », groupe IMA04
 
 Aleksandr SHIRKUNOV <aleksandr.shirkunov@etu-univ-grenoble-alpes.fr>
-Arna BARLUBAYEVA <arna.barlubayeva@univ-grenoble-alpes.fr>
+Arna BARLUBAYEVA <arna.barlubayeva@etu-univ-grenoble-alpes.fr>
 -----------------------------------------------------------------------------
 """
 
 # IMPORTS ######################################################################
 
 from pathlib import Path  # gestion fichiers
-
+import tkinter as tk
 
 # CONSTANTES ###################################################################
 
@@ -21,6 +21,13 @@ TAILLE_PLATEAU = 15  # taille du plateau de jeu
 TAILLE_MARGE = 4  # taille marge gauche (qui contient les numéros de ligne)
 
 JOKER = '?'  # jeton joker
+
+BONUS_SYMBOLS = {
+    "MT": '!',
+    "MD": '@',
+    "LT": '#',
+    "LD": '$'
+}
 
 # ⚠ pas de variable globales, sauf cas exceptionnel
 
@@ -38,10 +45,10 @@ def symetrise_liste(lst) :
     [1, 2, 3, 2, 1]
     """
     copie_lst = list(lst)
-    for i in range(2, len(copie_lst)+1) : lst.append(copie_lst[-i])
+    for i in range(2, len(copie_lst)+1): lst.append(copie_lst[-i])
 
 
-def init_bonus() :
+def init_bonus():
     """
     Q1) Initialise le plateau des bonus.
     """
@@ -60,31 +67,94 @@ def init_bonus() :
         ['MT', ''  , ''  , 'LD', ''  , ''  , ''  , 'MD']
     ]
     # On transforme les demi-lignes du plateau en lignes :
-    for ligne in plt_bonus : symetrise_liste(ligne)
+    for ligne in plt_bonus: symetrise_liste(ligne)
     # On transforme le demi-plateau en plateau :
     symetrise_liste(plt_bonus)
 
     return plt_bonus
 
 
+def init_jetons():
+    """
+    Q2) Initialise le plateau des jetons.
+    """
+    plt_jetons = [['' for j in range(TAILLE_PLATEAU)] for i in range(TAILLE_PLATEAU)]
+    return plt_jetons
+
+
+def affiche_jetons(jetons, bonus):
+    """
+    Q3) Affiche le plateau des jetons.
+    """
+    for ligne in range(TAILLE_PLATEAU):
+        print("|---" * TAILLE_PLATEAU + '|')
+        for col in range(TAILLE_PLATEAU):
+            cur_char = jetons[ligne][col] or ' '
+            cur_bonus = BONUS_SYMBOLS.get(bonus[ligne][col], ' ')
+            print(f"| {cur_char}{cur_bonus}", end = '')
+        print('|')
+    print("|---" * TAILLE_PLATEAU + '|')
+
+    
+def affiche_jetons_gui(jetons, bonus, cell_size):
+    """
+    Q6) Affiche le plateau des jetons dans
+    
+    """
+    root = tk.Tk()
+    root.title("Le Scrabble")
+    
+    canvas = tk.Canvas(root,
+                       width = cell_size*TAILLE_PLATEAU,
+                       height = cell_size*TAILLE_PLATEAU)
+    canvas.pack()
+
+    for ligne in range(TAILLE_PLATEAU):
+        for col in range(TAILLE_PLATEAU):
+            x1 = ligne * cell_size
+            y1 = col * cell_size
+            x2 = ligne * cell_size + cell_size
+            y2 = col * cell_size + cell_size
+
+            cur_bonus = bonus[ligne][col]
+            color = "white"
+            match cur_bonus:
+                case "MT": color = "red"
+                case "MD": color = "yellow"
+                case "LT": color = "blue"
+                case "LD": color = "green"
+            canvas.create_rectangle(x1, y1,
+                                    x2, y2,
+                                    fill = color,
+                                    outline = "black")
+
+            cur_char = jetons[ligne][col]
+            color = "black"
+            canvas.create_text((x1 + x2) / 2,
+                               (y1 + y2) / 2,
+                               fill = "black",
+                               font = ("Arial", int(cell_size / 2)),
+                               text = cur_char)
+    root.mainloop()
+    
 # PARTIE 3 : CONSTRUCTIONS DE MOTS #############################################
 
-def generer_dictfr(nf='littre.txt') :
+def generer_dictfr(nf = 'littre.txt'):
     """Liste des mots Français en majuscules sans accent.
 
     >>> len(generer_dictfr())
     73085
     """
     mots = []
-    with Path(nf).open(encoding='utf_8') as fich_mots :
-        for line in fich_mots : mots.append(line.strip().upper())
+    with Path(nf).open(encoding='utf_8') as fich_mots:
+        for line in fich_mots: mots.append(line.strip().upper())
     return mots
 
 
 # PARTIE 4 : VALEUR D'UN MOT ###################################################
 
 
-def generer_dico() :
+def generer_dico():
     """Dictionnaire des jetons.
 
     >>> jetons = generer_dico()
@@ -100,7 +170,21 @@ def generer_dico() :
     """
     jetons = {}
     with Path('lettres.txt').open(encoding='utf_8') as lettres :
-        for ligne in lettres :
+        for ligne in lettres:
             l, v, o = ligne.strip().split(';')
             jetons[l] = {'occ': int(o), 'val': int(v)}
     return jetons
+
+
+# MAIN PROGRAM  ################################################################
+
+if __name__ == "__main__":
+    # Q3: genere et affiche le plateau
+    jetons = init_jetons()
+    jetons[0][0] = 'B'
+    jetons[1][1] = 'C'
+    jetons[2][3] = 'A'
+    jetons[14][14] = 'E'
+    bonus = init_bonus()
+    affiche_jetons(jetons, bonus)
+    affiche_jetons_gui(jetons, bonus, 50)
