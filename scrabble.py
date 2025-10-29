4ebc4e7f461324e49f08b414418c05382f5051c1
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-29 13:54:33 +0100

    [PART 3] implemented mots_jouables()
    fix: echanger() now accounts for duplicate letters in hand
    
    feat: implemented mot_jouable()
    
    feat: mot_jouable() now accounts for joker tokens
    
    feat: mot_jouable() now returns True if you can complete the word within a margin of error of 'nombre_manq' characters

--- scrabble.py
+++ scrabble.py
@@ -30,9 +30,6 @@ BONUS_SYMBOLS = {
     "LD": '$'
 }
 
-# ⚠ pas de variable globales, sauf cas exceptionnel
-
-
 # PARTIE 1 : LE PLATEAU ########################################################
 
 def symetrise_liste(lst) :
@@ -47,7 +44,6 @@ def symetrise_liste(lst) :
     """
     copie_lst = list(lst)
     for i in range(2, len(copie_lst)+1): lst.append(copie_lst[-i])
-
 
 def init_bonus():
     """
@@ -74,14 +70,12 @@ def init_bonus():
 
     return plt_bonus
 
-
 def init_jetons():
     """
     Q2) Initialise le plateau des jetons.
     """
     plt_jetons = [['' for j in range(TAILLE_PLATEAU)] for i in range(TAILLE_PLATEAU)]
     return plt_jetons
-
 
 def affiche_jetons(jetons, bonus):
     """
@@ -95,7 +89,6 @@ def affiche_jetons(jetons, bonus):
             print(f"| {jeton_act}{bonus_act}", end = '')
         print('|')
     print("|---" * TAILLE_PLATEAU + '|')
-
     
 def affiche_jetons_gui(jetons, bonus, taille_cell):
     """
@@ -137,7 +130,6 @@ def affiche_jetons_gui(jetons, bonus, taille_cell):
                                font = ("Arial", int(taille_cell / 2)),
                                text = jeton_act)
     root.mainloop()
-
     
 # PARTIE 2 : LA PIOCHE #########################################################
 
@@ -151,7 +143,6 @@ def init_pioche_alea():
         pioche.append(nouv_let)
     return pioche
 
-
 def piocher(x, sac):
     """
     Q8) pioche le sac pour x jetons.
@@ -162,7 +153,6 @@ def piocher(x, sac):
         jetons_pioches.append(sac.pop(pos_alea))
     return jetons_pioches
 
-
 def completer_main(main, sac):
     """
     Q9) complete la main a 7 jetons.
@@ -170,29 +160,29 @@ def completer_main(main, sac):
     nombre_jetons = min(7 - len(main), len(sac))
     main += piocher(nombre_jetons, sac)
 
-
 def echanger(jetons, main, sac):
     """
     Q10) echange les jetons dans la main avec des nouveaux jetons
     pris dans le sac.
     """
-    nombre_jetons = len(jetons)
+    nombre_remp = len(jetons)
     remplacer = [] # les positions des jetons qu'on doit remplacer dans la main
     for jeton in jetons:
         if jeton not in main:
             return False
         else:
-            remplacer.append(main.index(jeton))
-    if len(sac) < nombre_jetons or len(sac) == 0:
+            ind_remplacer = main.index(jeton)
+            remplacer.append(ind_remplacer)
+            main[ind_remplacer] = ''
+    if len(sac) < nombre_remp or len(sac) == 0:
         return False
 
-    jetons_pioches = piocher(nombre_jetons, sac)
-    for i in range(nombre_jetons):
+    jetons_pioches = piocher(nombre_remp, sac)
+    for i in range(nombre_remp):
         main[remplacer[i]] = jetons_pioches[i]
         
     sac += jetons
     return True
-
 
 # PARTIE 3 : CONSTRUCTIONS DE MOTS #############################################
 
@@ -207,6 +197,58 @@ def generer_dictfr(nf = 'littre.txt'):
         for line in fich_mots: mots.append(line.strip().upper())
     return mots
 
+def select_mot_initiale(mots_fr, let):
+    """
+    Q13)
+    """
+    res = []
+    for mot in mots_fr:
+        if mot[0] == let: res.append(mot)
+    return res
+
+def select_mot_longueur(mots_ft, lgr):
+    """
+    Q14)
+    """
+    res = []
+    for mot in mots_fr:
+        if len(mot) == lgr: res.append(mot)
+    return res
+
+def mot_jouable(mot, ll, nombre_manq):
+    """
+    Q15)
+    """
+    tmp = [let for let in ll]
+    let_manq = 0
+    for let in mot:
+        let_trouve = False
+        for i in range(len(tmp)):
+            if let == tmp[i]:
+                tmp.pop(i)
+                let_trouve = True
+                break
+        if not let_trouve:
+            let_manq += 1
+
+    nombre_jokers = 0
+    for let in tmp:
+        if let == '?':
+            nombre_jokers += 1
+    
+    if let_manq - nombre_jokers > nombre_manq:
+        return False
+    return True
+
+def mots_jouables(mots_fr, ll, nombre_manq):
+    """
+    Q16)
+    """
+    res = []
+    for mot in mots_fr:
+        if mot_jouable(mot, ll, nombre_manq):
+            res.append(mot)
+    return res
 
 # PARTIE 4 : VALEUR D'UN MOT ###################################################
 
@@ -243,7 +285,7 @@ if __name__ == "__main__":
     bonus = init_bonus()
     affiche_jetons(jetons, bonus)
     affiche_jetons_gui(jetons, bonus, 50)
-
+    
     """
     Q11: test des fonctions de la partie 2.
     """
@@ -251,6 +293,7 @@ if __name__ == "__main__":
     joueur1_main = piocher(7, sac)
     joueur2_main = piocher(7, sac)
 
+    print(f"DEBUG: sac length is {len(sac)}\n")
     # commence le jeu pour j1
     print("Joueur 1, votre main est: ", end = '')
     for jeton in joueur1_main:
@@ -279,4 +322,27 @@ if __name__ == "__main__":
     print("Joueur 2, votre main est: ", end = '')
     for jeton in joueur2_main:
         print(f"{jeton} ", end = '')
+    print("\n")
+    print(f"DEBUG: sac length is {len(sac)}\n")
+
+    
+    """
+    Q12)
+    """
+    mots_fr = generer_dictfr("littre.txt")
+    print(len(mots_fr))
+    for mot in mots_fr:
+        if mot[0] == 'U': print(mot)
+
+    """
+    Q13-14)
+    """
+    print(len(select_mot_initiale(mots_fr, 'Y')))
+    print(len(select_mot_longueur(mots_fr, 19)))
     print()
+    
+    """
+    Q15-17)
+    """
+    print(mot_jouable("STEGANOGRAPHIE", list("PARTIES"), 1))
+    print(mots_jouables(mots_fr, list("PARTIES"), 1))
